d9cfcf38c669184b9c518ab9df972be9a9c15608
Joshi <rohit.d.joshi@capgemini.com>
2017-01-12 15:36:45 +0530

    Compiled my code

--- chaincode_start.go
+++ chaincode_start.go
@@ -1,3 +1,5 @@
+package Test
+
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
     if len(args) != 1 {
         return nil, errors.New("Incorrect number of arguments. Expecting 1")
